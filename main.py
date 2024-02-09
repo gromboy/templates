baf763f1179609f6f9e72a1fd4947e2403e4b248
CAD-16 <timazaharov07@gmail.com>
2024-02-09 19:18:46 +0400

    2

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = "Ученик Яндекс.Лицея"
+@app.route('/training/<prof>')
+def index(prof):
     return render_template('page.html', title='Домашняя страница',
-                           username=user)
+                           prof=prof)
 
 
 app.run(port=8080, host='127.0.0.1')
