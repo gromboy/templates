7ed407648b08b49d29a23d5837d96b38a6f6fcd7
CAD-16 <timazaharov07@gmail.com>
2024-02-09 19:24:01 +0400

    with index

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Домашняя')
+
+
 @app.route('/training/<prof>')
 def index(prof):
     return render_template('page.html', title='Домашняя страница',
